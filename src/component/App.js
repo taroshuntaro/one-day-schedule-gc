43c8f667bd62bbdbf13b3af47ea1108bfcab6953
taroshuntaro <160341ss@gmail.com>
2020-05-20 01:22:17 +0900

    update design html & react component

--- src/component/App.js
+++ src/component/App.js
@@ -1,21 +1,51 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TodayIcon from '@material-ui/icons/Today';
+import { teal } from '@material-ui/core/colors';
 
-function App() {
+const uiStyle = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  todayIcon: {
+    marginRight: theme.spacing(1),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBar: {
+    backgroundColor: teal['A700'],
+  },
+}));
+
+export default function App() {
+  const classes = uiStyle();
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>さんぷるてきすと</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <NavBarComponent></NavBarComponent>
     </div>
   );
 }
 
-export default App;
+function NavBarComponent() {
+  const classes = uiStyle();
+
+  return (
+    <AppBar position='static' className={classes.appBar}>
+      <Toolbar>
+        <TodayIcon className={classes.todayIcon}></TodayIcon>
+        <Typography variant='h6' className={classes.title}>
+          2020年5月11日（月）
+        </Typography>
+        <Button color='inherit' id='signout_button'>
+          Sign out
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
