297f54fb1c298ec940beb25ca58267608caa6dfb
taroshuntaro <160341ss@gmail.com>
2020-05-21 02:13:07 +0900

    clean

--- src/component/MainContainerComponent.jsx
+++ src/component/MainContainerComponent.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 /** Material-UI module */
-//import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { grey } from '@material-ui/core/colors';
 
+const uiStyle = makeStyles((theme) => ({
+  container: { backgroundColor: grey[200], minHeight: '100vh' },
+}));
+
 export default function MainContainerComponent() {
-  //const classes = uiStyle();
+  const classes = uiStyle();
 
   return (
-    <Container
-      maxWidth='md'
-      style={{ backgroundColor: grey[200], minHeight: '100vh' }}
-    >
+    <Container maxWidth='md' className={classes.container}>
       sample
     </Container>
   );
